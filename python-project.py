bb719227b8b825633b5a9bb836dcadc8183042db
kadir-zeren <k4dzeren@gmail.com>
2024-08-27 18:07:05 +0300

    ptoject-1

--- python-project.py
+++ python-project.py
@@ -1,1 +1,55 @@
 print('Hello world')
+print('Benim yazdigim ilk kod')
+print('Hello World!')
+print("Hello World!")
+print('Hello World!')
+print("Clarusway School!")
+print("I'm happy to learn!")
+print("Cift tirnak yazdim")
+print('''uc tirnak ile yazdim''')
+print("bir cumle")
+print("I'm great")
+print ('"hi" he said')
+print ('''Don't do it" said the manager''')
+print('ben bunu yazmak istiyorum')
+print('''bu birinci satir
+bu ikinci satir
+bu uc
+bu dort''') 
+print('''bu birinci 'satir
+bu ikinci satir"
+bu uc
+bu dort''')
+print('3.14')
+print(3.14)
+print('3.14'*3)
+print (3.14*3)
+print(''''We should have enough time for our family"''')
+print('bir')
+print()
+print('uc')
+print('first line')
+print()
+print('''third line''')
+print()
+print('')
+print()
+print("")
+print('I\'m great')
+print("\"don't do it\" said the manager")
+print('bu benim cumlem \nbu ikinci satir')
+print('bu benim cumlem \tbu ikinci satir')
+print("ben kodlarkjlj vl k k k kk 11 lkjlkjlkjljlk\
+kdkdkdkdkdkd\
+kdkdkdkdkd")
+print('being a good person')
+#buradaki fonksiyon kare alma fonksiyonu
+print('kullanici ismi') # Burada kullanici ismi yazddiriliyor.
+x = 5 # burada x degeri atandi
+# This is a single line comment
+print('hello') # This is an inline comment
+print('hello')
+# First multi-line comment
+# Second multi-line comment
+# Third multi-line comment
+print("Hello, world!")
